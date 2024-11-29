9624bf2619b97e6e55d29a9874b47d9a8073c557
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-11-29 19:08:13 +0530

    new project with spring boot using REST apis

--- springbootweb1/src/main/java/com/cadt/HomeController.java
+++ springbootweb1/src/main/java/com/cadt/HomeController.java
@@ -3,23 +3,24 @@ package com.cadt;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest request, HttpSession session){
-        int num1 = Integer.parseInt(request.getParameter("num1"));
-        int num2 = Integer.parseInt(request.getParameter("num2"));
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
         int result = num1 + num2;
-        session.setAttribute("result", result);
-        return "result.jsp";
+        model.addAttribute("result", result);
+        return "result";
     }
 }
--- springbootweb1/src/main/webapp/views/result.jsp
+++ springbootweb1/src/main/webapp/views/result.jsp
@@ -8,7 +8,7 @@
 
 <body>
 
-<h1>Result is : <%= session.getAttribute("result") %></h1>
+<h1>Result is : ${result}</h1>
 
 
 </body>
