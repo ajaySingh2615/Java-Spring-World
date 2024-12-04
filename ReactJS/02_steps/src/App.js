14838c2ddec3b141e3bfe3ccb4c445b35adc7781
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-12-04 17:30:23 +0530

    updated app with multiply styles and themes

--- ReactJS/02_steps/src/App.js
+++ ReactJS/02_steps/src/App.js
@@ -24,27 +24,29 @@ export default function App() {
         &times;
       </button>
       {isOpen && (
-        <div className="steps">
+        <div className="steps fade-in">
           <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
+            <div className={`number ${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`number ${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`number ${step >= 3 ? "active" : ""}`}>3</div>
           </div>
 
-          <p className="message">
+          <p className="message slide-in">
             Step {step}: {messages[step - 1]}
           </p>
 
           <div className="buttons">
             <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              className="button"
               onClick={handlePrevious}
+              disabled={step === 1}
             >
               Previous
             </button>
             <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              className="button"
               onClick={handleNext}
+              disabled={step === 3}
             >
               Next
             </button>
