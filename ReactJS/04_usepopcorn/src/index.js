c82f5de72b4b6342be1bd32f807751654c336d31
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-12-08 19:31:43 +0530

    using an async function to fetch the data

--- ReactJS/04_usepopcorn/src/index.js
+++ ReactJS/04_usepopcorn/src/index.js
@@ -1,29 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./StarRating";
-// import App from "./App";
-// import "./index.css";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} star.</p>
-    </div>
-  );
-}
+import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terriable", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" defaultRating={3} />
-    <Test />
+    <App />
   </React.StrictMode>
 );
